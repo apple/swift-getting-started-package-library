4ae22da07cb6034f76e6e99265998c61f66df743
Tim Condon <0xTim@users.noreply.github.com>
2023-08-30 00:27:58 +0100

    Update for 5.8 guide (#1)

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -13,17 +13,17 @@
 import Foundation
 
 struct Email: CustomStringConvertible {
-    var description: String
+  var description: String
 
-    public init(_ emailString: String) throws {
-        let regex = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}"#
-        guard let _ = emailString.range(of: regex, options: .regularExpression) else {
-            throw InvalidEmailError(email: emailString)
-        }
-        self.description = emailString
+  public init(_ emailString: String) throws {
+    let regex = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}"#
+    guard let _ = emailString.range(of: regex, options: .regularExpression) else {
+      throw InvalidEmailError(email: emailString)
     }
+    self.description = emailString
+  }
 }
 
 private struct InvalidEmailError: Error {
-    let email: String
+  let email: String
 }
