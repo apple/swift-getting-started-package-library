a4360e829c6ede69e00d57a5b5dad0c579e5c32f
Tim <0xtimc@gmail.com>
2023-08-29 19:30:37 +0100

    Update for 5.8 guide

--- Package.swift
+++ Package.swift
@@ -1,18 +1,21 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyLibrary",
             targets: ["MyLibrary"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary",
-            dependencies: []),
+            name: "MyLibrary"),
         .testTarget(
             name: "MyLibraryTests",
             dependencies: ["MyLibrary"]),
--- Tests/MyLibraryTests/MyLibraryTests.swift
+++ Tests/MyLibraryTests/MyLibraryTests.swift
@@ -10,14 +10,14 @@
 //
 //===--------------------------------------------------------------------
 
-import XCTest
 @testable import MyLibrary
+import XCTest
 
 final class MyLibraryTests: XCTestCase {
-    func testExample() throws {
-        let email = try Email("john.appleseed@apple.com")
-        XCTAssertEqual(email.description, "john.appleseed@apple.com")
+  func testEmail() throws {
+    let email = try Email("john.appleseed@apple.com")
+    XCTAssertEqual(email.description, "john.appleseed@apple.com")
 
-        XCTAssertThrowsError(try Email("invalid"))
-    }
+    XCTAssertThrowsError(try Email("invalid"))
+  }
 }
